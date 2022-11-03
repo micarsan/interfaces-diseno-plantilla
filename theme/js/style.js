9f515d67ffc021e8aefc9540557eb80e6c2c41ba
Ivan Marquez <ivanaurelio.s258702@cesurformacion.com>
2022-11-03 10:22:41 +0100

    Añadido botones para cambio de imagen en pantalla completa

--- theme/js/style.js
+++ theme/js/style.js
@@ -1,28 +1,28 @@
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
     // Blur efect when focus on header search input and animations
-    document.querySelector('#searchbox input').addEventListener("focus", function(){
-        
-        let header_h1 =  document.querySelector('header .header_top h1');
-        
+    document.querySelector('#searchbox input').addEventListener("focus", function () {
+
+        let header_h1 = document.querySelector('header .header_top h1');
+
         header_h1.classList.add('blur');
 
         let available_width = header_h1.offsetWidth + 74;
-        let input_width = available_width/2;
-        let input_margin = available_width/4;
-        
-        if( available_width < 550 ) {
-            input_width = available_width-25;
+        let input_width = available_width / 2;
+        let input_margin = available_width / 4;
+
+        if (available_width < 550) {
+            input_width = available_width - 25;
             input_margin = 0;
         }
 
         document.querySelector('header #searchbox input').style.width = input_width + 'px';
-        document.querySelector('header #searchbox input').style.marginRight = input_margin +'px';
+        document.querySelector('header #searchbox input').style.marginRight = input_margin + 'px';
     });
-    
-    document.querySelector('#searchbox input').addEventListener("focusout", function(){
-        
+
+    document.querySelector('#searchbox input').addEventListener("focusout", function () {
+
         document.querySelector('header .header_top h1').classList.remove('blur');
 
         document.querySelector('header #searchbox input').style.width = '';
@@ -30,10 +30,10 @@ window.addEventListener("load", function(){
 
     });
 
-    
+
 
     // User box
-    document.querySelector('header #userbox>a').addEventListener('click', function(){
+    document.querySelector('header #userbox>a').addEventListener('click', function () {
         this.parentNode.classList.toggle('active');
     });
 
@@ -43,74 +43,150 @@ window.addEventListener("load", function(){
 
     // product gallery full screen image
     let images = document.querySelectorAll('.product-gallery img');
-    for( let image of images ) {
-        image.addEventListener('click', function(){
-            overlay(true);
+    for (let i = 0; i < images.length; i++) {
+        images[i].addEventListener('click', function () {
+            full_screen_image(i);
+    });
 
-            let container = document.createElement('div');
-            container.id='full-screen-image';
-            
-            let img = document.createElement('img');
-            img.setAttribute('src', image.getAttribute('src'));
-            img.setAttribute('style', 'height: 95%; margin: auto;')
-            
-            container.appendChild(img);
-            document.body.appendChild(container);
+    }
+});
 
-            setTimeout(() => {
-                container.classList.add('show');
-            }, 10);
+/**
+ * Create and insert image in full screen
+ * Need index of array
+ */
+function full_screen_image(index_key) {
 
-            container.addEventListener('click', function(){
-                overlay(false);
-                container.remove();
-            });
+    if (document.querySelector('#full-screen-image') ) {
+        document.querySelector('#full-screen-image button.left').remove();
+        document.querySelector('#full-screen-image button.right').remove();
+        document.querySelector('#full-screen-image img').remove();
+        
+        var container = document.querySelector('#full-screen-image');
+    } else {
+        var container = document.createElement('div');
+        container.id = 'full-screen-image';
+    }
+
+    let images = document.querySelectorAll('.product-gallery img');
+
+    let image = images[index_key];
+    
+    if (!document.querySelector('#overlay_image')) {
+        let overlay_image = document.createElement('div');
+        overlay_image.id = 'overlay_image';
+        overlay_image.classList.add('overlay');
+        overlay_image.classList.add('show');
+
+        container.appendChild(overlay_image);
+    }
+    
+
+    let left_button = document.createElement('button');
+    left_button.classList.add('left');
+    left_button.innerHTML = '&#10132;';
+
+    if( index_key > 0 ) {
+        left_button.addEventListener('click', function(){
+            full_screen_image(index_key-1);
         });
     }
 
+    let right_button = document.createElement('button');
+    right_button.classList.add('right');
+    right_button.innerHTML = '&#10132;';
+    
+    if( index_key < images.length-1 ) {
+        right_button.addEventListener('click', function(){
+            full_screen_image(index_key+1);
+        });
+    }
 
-});
+    let img = document.createElement('img');
 
-window.addEventListener("resize", function(){
+    img.setAttribute('src', image.getAttribute('src'));
+    img.setAttribute('style', 'height: 95%; margin: auto; z-index:15;');
+
+    container.appendChild(left_button);
+    container.appendChild(img);
+    container.appendChild(right_button);
+
+    document.body.appendChild(container);
+
+    setTimeout(() => {
+        container.classList.add('show');
+        full_screen_image_buttons();
+    }, 10);
+
+    
+    
+    document.querySelector('.overlay.show').addEventListener('click', function () {
+        console.log('overlay click');
+        overlay(false);
+        container.remove();
+    });
+}
+
+/**
+ * Insert back and next buttons on full screen image
+ */
+function full_screen_image_buttons() {
+    let image = document.querySelector('#full-screen-image img');
+    if (image) {
+        let position = image.getBoundingClientRect();
+        console.log(position)
+        let positionleft = position.left + window.scrollX;
+        let positionright = position.right;
+        let izquierda = document.querySelector('#full-screen-image button.left');
+        let right = document.querySelector('#full-screen-image button.right');
+        let width = image.offsetWidth;
+        izquierda.setAttribute('style', 'left: ' + (positionleft - 25) + 'px; z-index:20;')
+        right.setAttribute('style', 'right: ' + (positionright - width - 25) + 'px; z-index:20;')
+    }
+
+}
+
+window.addEventListener("resize", function () {
     // product galleries
     product_gallery();
+    full_screen_image_buttons();
 });
 
-function product_gallery(){
-    
+function product_gallery() {
+
     // Landscape Galleries
     let galleries = document.querySelectorAll('.product-gallery');
-    for( let gallery of galleries ) {
+    for (let gallery of galleries) {
 
         let scrool_width = gallery.querySelector('img').offsetWidth + 20;
 
         // Image width (full width in mobile portrait)
         let image_width = gallery.querySelector('div').offsetWidth - 4;
-        if( image_width < 500 ) {
+        if (image_width < 500) {
 
             // Check and create if <style data-gallery> not exist
-            if( !document.querySelector('style[data-gallery]') ) {
+            if (!document.querySelector('style[data-gallery]')) {
                 let style = document.createElement('style');
                 style.setAttribute('data-gallery', '');
-                document.head.appendChild( style );
+                document.head.appendChild(style);
             }
-            
+
             let images = gallery.querySelectorAll('img');
             document.querySelector('style[data-gallery]').textContent = '.product-gallery.landscape div div>img, .product-gallery.portrait div div>img { width: ' + image_width + 'px; } ';
-            
+
             scrool_width = image_width + 20;
         } else {
-            if( document.querySelector('style[data-gallery]') ) {
+            if (document.querySelector('style[data-gallery]')) {
                 document.querySelector('style[data-gallery]').remove();
             }
         }
-        
+
         // buttons
-        gallery.querySelector('button.left').addEventListener('click', function(){
+        gallery.querySelector('button.left').addEventListener('click', function () {
             gallery.querySelector('div').scrollLeft += -scrool_width;
         });
 
-        gallery.querySelector('button.right').addEventListener('click', function(){
+        gallery.querySelector('button.right').addEventListener('click', function () {
             gallery.querySelector('div').scrollLeft += scrool_width;
         });
 
@@ -122,8 +198,8 @@ function product_gallery(){
 /**
  * Show or hide overlay layer
  */
-function overlay( status ) {
-    if( status ) {
+function overlay(status) {
+    if (status) {
         document.getElementById('overlay').classList.add('show');
     } else {
         document.getElementById('overlay').classList.remove('show');
