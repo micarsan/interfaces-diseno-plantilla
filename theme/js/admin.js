ce9dacd8f0c328d4d23f5cf4837db38e41689206
micarsan <miguelangel.s@260606@cesurformacion.com>
2022-11-29 22:39:09 +0100

    Detalles de la administración

--- theme/js/admin.js
+++ theme/js/admin.js
@@ -1,5 +1,10 @@
 window.addEventListener("load", function () {
+    
     document.querySelector('#content aside div.button').addEventListener('click', function() {
         document.querySelector('#content aside').classList.toggle('active');
     });
+
+
+
+
 });
